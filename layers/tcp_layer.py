1478cd9606749a88c80157d10c0fcf9535ba0270
kanaknsen <114247720+kanaknsen@users.noreply.github.com>
2024-06-21 11:24:19 -0400

    Update tcp_layer.py

--- layers/tcp_layer.py
+++ layers/tcp_layer.py
@@ -133,6 +133,57 @@ class TCPLayer(Layer):
         # Dataofs is a 4 bit header, so a max of 15
         return random.randint(1, 15)
 
+    def tcptraceroute(target, port, n, timeout, maxhops):
+	try:
+		ip_target = socket.gethostbyname(target)
+	except socket.error as e:
+		print "Socket error: %s" % e
+		sys.exit(1)
+
+	print "traceroute (%s) to %s (%s), port %d, %d hops max" % ("TCP", target, ip_target, port, maxhops)
+
+	for ttl in range(1, maxhops):
+		# s1 is the sender, s2 is the receiver
+		s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s1.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
+		s1.settimeout(timeout)
+
+		s2 = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+		s2.settimeout(timeout)
+		s2.bind(("0.0.0.0", port))
+
+		try:
+			# connect is enough; don't need to send anything
+			s1.connect((ip_target, port))
+		except socket.timeout as e:
+			output(ttl)
+			continue
+		except socket.error as e:
+			if e.errno == socket.errno.ECONNREFUSED:
+				# Connection refused, which means target was reached
+				output(ttl, ip_target, n, "closed")
+				break
+
+			# retrieve "Destination unreabable" packet
+			try:
+				data, addr = s2.recvfrom(1024)
+				ip_hop = addr[0]
+			except socket.error as e:
+				# received nothing
+				ip_hop = ""
+			if ip_hop:
+				output(ttl, ip_hop, n)
+			else:
+				output(ttl)
+			continue
+		finally:
+			s1.close()
+			s2.close()
+
+		output(ttl, ip_target, n, "open")
+		break
+
+
     def gen_flags(self, field):
         """
         Generates a random set of flags. 50% of the time it picks randomly from
